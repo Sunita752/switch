6c93065696724cf2128d2ba6a0fbe15603b64335
Sunita752 <74800878+Sunita752@users.noreply.github.com>
2020-12-10 16:18:10 -0600

    Add files via upload

---
+++ Switchstatement/src/Homework.java
@@ -0,0 +1,29 @@
+
+public class Homework {
+
+	public static void main(String[] args) {
+	
+		char grade = 'A';
+		
+		switch(grade) {
+		case 'A':
+		System.out.println("You made an A. Excellent job!");
+		break;
+		case 'B':
+			System.out.println("You made a B . Good job!");
+			break;
+		case 'C':
+			System.out.println("You made a C. You did okay.");
+			break;
+		case 'D':
+			System.out.println("You made a D. You did it poor");
+			break;
+			
+		
+		}
+		
+	
+
+	}
+
+}
